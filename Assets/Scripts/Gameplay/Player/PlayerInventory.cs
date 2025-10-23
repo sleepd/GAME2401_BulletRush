5c730117bc565a92addc191e1c23c0a474bdd9d0
Qiao Xu <sleeepd@gmail.com>
2025-10-23 04:28:11 -0600

    WIP

---
+++ Assets/Scripts/Gameplay/Player/PlayerInventory.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayerInventory : PlayerComponent
+{
+    public PlayerInventory(PlayerController playerController) : base(playerController)
+    {
+    }
+
+    public override void OnDisable()
+    {
+        
+    }
+
+    public override void OnEnable()
+    {
+        
+    }
+
+    public override void Update()
+    {
+        
+    }
+}
